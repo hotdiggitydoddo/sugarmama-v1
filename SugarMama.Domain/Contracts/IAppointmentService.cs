158f2e0d1d2f6b1d99f4e4fca2774ae79645a346
hotdiggitydoddo <hotdiggitydoddo@gmail.com>
2013-12-22 19:06:57 -0800

    Add EF and core objects

---
+++ SugarMama.Domain/Contracts/IAppointmentService.cs
@@ -0,0 +1,18 @@
+﻿using SugarMama.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SugarMama.Domain.Contracts
+{
+    public interface IAppointmentService
+    {
+        Appointment BookAppointment(Appointment appointment);
+        bool CancelAppointment(int id);
+        Appointment EditAppointment(int id, Appointment editedAppointment);
+        IList<Appointment> GetAllAppointments();
+        Appointment GetAppointment(int id);
+    }
+}
---
+++ SugarMama.Model/Appointment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SugarMama.Model
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime Time { get; set; }
+        public float Duration { get; set; }
+        public bool RemindMeViaTxt { get; set; }
+    }
+}
